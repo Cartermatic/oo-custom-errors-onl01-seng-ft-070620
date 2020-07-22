ad67fd47ab58ad56fb015e385e18c033c16f7c8e
Carter Parrett <cparrett94@hotmail.com>
2020-07-22 22:38:30 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,19 +1,20 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
- if person.class != Person
+    if person.class != Person
       raise PartnerError
     else
       person.partner = self
     end
   end
-class PartnerError < StandardError
+ 
+  class PartnerError < StandardError
   end
 end
 
